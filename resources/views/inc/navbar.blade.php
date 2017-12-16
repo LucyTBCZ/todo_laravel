75f0c0075b36ad8b9664d1ae7611a74aadbcbe57
LucyTBCZ <lumnesty@gmail.com>
2017-12-16 18:23:19 +0100

    commit video 5 Models & Database Migrations

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -8,7 +8,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="/">{{config('app.name', 'todo_laravel')}}</a>
+        <a class="navbar-brand" href="/">{{config('app.name', 'ToDo_Laravel')}}</a>
       </div>
       <div id="navbar" class="collapse navbar-collapse">
         <ul class="nav navbar-nav">
